998e06e8250b3557a4cc9056b93811c0751dbb96
Aaron Berdanier <aaron.berdanier@gmail.com>
2015-11-04 07:41:06 -0500

    Update biodiversity

---
+++ server.R
@@ -0,0 +1,83 @@
+library(shiny)
+
+mythical<- read.csv("data/mythical.csv")
+m<- as.character(mythical[,1])
+
+getCommunity <- function(S=10,N=1000,equalprob=T){
+  if(S > 51) S <- 51 # right now we only have 26 species - each unique letter in the alphabet
+  prob <- rep(1/S,S)
+  if(!equalprob){
+    Nx <- rpois(S,sample(1:S,1))
+    prob <- Nx/sum(Nx)    
+  }
+  sub<-m[sample(S,N,replace=T,prob=prob)]
+  sub
+}
+
+shinyServer(function(input, output){
+  # output for Q1     
+  output$ansQ1<- reactive({
+    validate( need(input$Q1, "Select an answer") )
+    if (input$Q1==3){answer<- "Correct! Richness is proportional to the number of species."}
+    else{answer<-"Incorrect"}
+    answer
+  })
+  #Output for Q2 multiple choice 
+  output$ansQ2<- reactive({
+    validate( need(input$Q2, "Select an answer") )
+    if (input$Q2==1){answer<- "Correct! Why do you think this happens?"}
+    else{answer<-"Incorrect"}
+    answer
+  })
+  
+  S<-reactive(input$num_species)
+  N<-reactive(input$num_individuals)
+  ran<- reactive({
+    if (input$random == "yes") {TRUE}
+    else {FALSE}
+  })
+  
+  values <- reactiveValues( df=matrix(NA,nrow=0,ncol=4) )
+  #create barplot based on values chosen by slider input
+  species <- reactive( getCommunity(S(),N(),ran()) )
+  output$dist= renderPlot({
+    par(mar=c(12,4,2,0.4))
+    barplot(table(species()), main="Species observations",xlab="", ylab="Number of Individuals",las=2)
+  })
+  
+  #calculate data metrics based on numbers chosen by slider input
+  metrics<- reactive({  
+    richness <- length(unique(species()))
+    Pi <- table(species())/length(species())
+    shannon <-round((-sum(Pi*log(Pi))),2)
+    #print(shannon)
+    lambda <- round(sum(Pi^2),2)
+    simpson <-lambda
+    num_s <- input$num_species
+    num_i <- input$num_individuals
+    x <- c(num_i,richness, shannon, simpson)
+    x
+  })
+  
+  #Add generated data from app to the table
+  adddata <- observeEvent(input$add,{       
+    newLine <- c(metrics()[1],metrics()[2],metrics()[3],metrics()[4])
+    values$df <- rbind(values$df, newLine)
+  })
+  
+  output$dataLine <- renderTable({
+    tmp <- matrix(unlist(metrics()))
+    rownames(tmp) <- c("N","Richness (S)","Shannon","Simpson")
+    colnames(tmp) <- NULL
+    tmp
+  })
+
+  #Output the data table comprised of data generated from the app
+  output$dataTable <- renderTable({
+    tmp<- values$df
+    colnames(tmp)<- c("N", "S", "Shannon", "Simpson")
+    rownames(tmp)<-  NULL
+    tmp
+  })
+  
+})
---
+++ ui.R
@@ -0,0 +1,39 @@
+library(shiny)
+
+shinyUI(
+  fluidPage(
+    fluidRow(
+      h4("Click 'Add To Table' to store values in the table."),
+      br(),
+      
+      #slider input
+      column(3, 
+        sliderInput('num_species', 'Number of Species to Examine: ', min=2, max=51, value=12),
+        #numeric input for number of individuals
+        numericInput('num_individuals', 'Number of individuals in the Population: ', 
+                     min=1000, max=10000, value=1000, step=50),
+        #buttons controlling "skewed" or "uniform" and "Get Community" and "Save to Table"
+        radioButtons("random", label= "Type of community distribution",
+                     choices=c("uniform"="yes", "skewed"="no"), selected="yes"),
+        actionButton("add", label= "Save to Table")
+      ),
+      
+      #creates space for multiple choice questions
+      column(5,
+        radioButtons("Q1", 'What happens to the richness if you have a very high number of species oberservations?', choices = c("It Decreases"=1, "It stays the same"=2,"It Increases"=3), selected=F),
+        h5(textOutput("ansQ1"), style="color:blue"),
+        #creates space for the distribution plot          
+        plotOutput("dist"),
+        tableOutput("dataLine")
+      ),
+      
+      #question 2 multiple choice 
+      column(4, 
+        radioButtons("Q2", 'What happens to the Simpson Index if you keep the number of species at 20 and increase the population size from very small to very large?', choices = c("It Decreases"=1, "It stays the same"=2,"It Increases"=3), selected=F),
+        h5(textOutput("ansQ2"), style="color:blue"),
+        #creates space for table output
+        tableOutput("dataTable")
+      )
+    )  
+  )
+)
